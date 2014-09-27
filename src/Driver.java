c87b5120d49f1e58d3d8e8988472879c0829c503
Enrique Gonzalez <egonzalez14@dons.usfca.edu>
2014-09-26 17:46:21 -0700

    Revert "Code Review"

--- src/Driver.java
+++ src/Driver.java
@@ -1,63 +1,8 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Driver {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO
-		String outputPath;
-		ArgumentParser ap = new ArgumentParser(args);
-		WordIndex wi = new WordIndex();
-		
-			if (ap.hasFlag("-d") && ap.hasValue("-d")){
-				String directoryPath = ap.getValue("-d");
-				try {
-					Path path = Paths.get(directoryPath);
-					RecursiveDirectoryStream.traverse(path,wi);
-					
-				}
-				catch (NoSuchFileException x) {
-				    System.err.format("%s: no such" + " file or directory%n", directoryPath);
-
-				} catch (IOException x) {
-				    System.err.format("%s%n", x);
-				}
-				
-				//changed traverse from private static void to static void
-
+	}
 	
-			}
-	
-			if (ap.hasFlag("-i") && !ap.hasValue("-i")){
-				String indexPathString = "index.txt";
-				try {
-					Path indexPath = Paths.get(".","index.txt");
-					Files.createFile(indexPath);
-
-					
-				}
-				catch (NoSuchFileException x) {
-				    System.err.format("%s: no such" + " file or directory%n",indexPathString);
-
-				} catch (IOException x) {
-				    System.err.format("%s%n", x);
-				}
-				
-
-			}
-			
-			
-			if (ap.hasFlag("-i") && ap.hasValue("-i")){
-				outputPath = ap.getValue("-i");
-				wi.print(outputPath);
-	
-			}
-			
-
-		}
 }
-	
-
